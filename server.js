9eed432bad3fb6cab4cfd4a81f241a32c39b1bf9
DCM <dcm@cs.uns.edu.ar>
2026-09-21 13:43:19 -0300

    actualizadas dependencias

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@as-integrations/express5');
 
 // Base de datos en memoria
 let actors = [
@@ -10,13 +12,20 @@ let actors = [
   { id: '5', name: 'Mark Ruffalo', movie: 'Avengers' },
   { id: '6', name: 'Tom Holland', movie: 'Spider-Man: Homecoming' },
   { id: '7', name: 'Zendaya', movie: 'Spider-Man: Homecoming' },
-  { id: '8', name: 'Robert Downey Jr.', movie: 'Avengers' }, // actor con más de una película
+  { id: '8', name: 'Robert Downey Jr.', movie: 'Avengers' },
   { id: '9', name: 'Benedict Cumberbatch', movie: 'Doctor Strange' },
-  { id: '10', name: 'Chadwick Boseman', movie: 'Black Panther' },
+  { id: '10', name: 'Chadwick Boseman', movie: 'Black Panther' }
 ];
 
+let nextId = 11;
+
+// A continuacion definimos esquema GraphQL y resolvers para manejar 
+// las consultas y mutaciones relacionadas con los actores. 
+// Lo idea es que esten en archivos separados, mejor modularizados, 
+// pero para simplificar el ejemplo los dejamos en el mismo archivo.
+
 // Definición del esquema GraphQL
-const typeDefs = gql`
+const typeDefs = `#graphql
   type Actor {
     id: ID!
     name: String!
@@ -36,45 +45,78 @@ const typeDefs = gql`
   }
 `;
 
-// Resolver
+// Resolvers
 const resolvers = {
   Query: {
     actors: () => actors,
-    actor: (_, { id }) => actors.find(a => a.id === id),
-    actorsByMovie: (_, { movie }) => actors.filter(a => a.movie === movie),
+
+    actor: (_, { id }) =>
+      actors.find(a => a.id === id),
+
+    actorsByMovie: (_, { movie }) =>
+      actors.filter(a => a.movie === movie)
   },
+
   Mutation: {
     addActor: (_, { name, movie }) => {
-      const newActor = { id: String(actors.length + 1), name, movie };
+      const newActor = {
+        id: String(nextId++),
+        name,
+        movie
+      };
+
       actors.push(newActor);
+
       return newActor;
     },
+
     updateActor: (_, { id, name, movie }) => {
       const actor = actors.find(a => a.id === id);
+
       if (!actor) return null;
+
       if (name !== undefined) actor.name = name;
       if (movie !== undefined) actor.movie = movie;
+
       return actor;
     },
+
     deleteActor: (_, { id }) => {
       const index = actors.findIndex(a => a.id === id);
+
       if (index === -1) return false;
+
       actors.splice(index, 1);
+
       return true;
-    },
-  },
+    }
+  }
 };
 
-// Crear servidor Apollo
-async function startServer() {
-  const app = express();
-  const server = new ApolloServer({ typeDefs, resolvers });
-  await server.start();
-  server.applyMiddleware({ app, path: '/graphql' });
 
-  app.listen({ port: 4000 }, () =>
-    console.log(`Servidor corriendo en http://localhost:4000/graphql`)
+// Crear servidor
+async function startServer() {
+
+  const app = express();
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+
+  await server.start();
+
+  app.use(
+    '/graphql',
+    express.json(),
+    expressMiddleware(server)
   );
+
+  app.listen(3000, () => {
+    console.log(
+      'Servidor corriendo en http://localhost:3000/graphql'
+    );
+  });
 }
 
 startServer();
